18b1d08fb9db1217138d1d5f6b106cbb77146c55
Vallabh Anwikar <vallabh.anwikar@gmail.com>
2025-08-04 21:19:45 +0530

    Commiting the current set of problems already solved in local directory

---
+++ heaps/ReachPileGift.java
@@ -0,0 +1,12 @@
+package heaps;
+
+public class ReachPileGift {
+    public static void main(String[] args) {
+
+    }
+
+    public int giftsAfterKSeconds(int[] gifts, int k)
+    {
+        return 0;
+    }
+}
